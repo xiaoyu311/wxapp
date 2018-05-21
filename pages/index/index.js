052cf3d8790640f2d5b5ad5db6f4fb495afb75bf
刘春雨 <liuchunyu@liuchunyudeMacBook-Pro.local>
2018-05-22 00:28:10 +0800

    添加文章添加页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,5 +3,18 @@
 const app = getApp()
 
 Page({
-
+  data: {
+    articleList: []
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://m.xxiaoyu.com/article/article_list', //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res)
+      }
+    })
+  }
 })
